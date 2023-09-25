7e3e9bc6b4bd32bbeb7a551863c8388dd8ecc466
johann9455 <johannencali@hotmail.com>
2023-09-25 14:17:24 -0500

    ejercicios

---
+++ Ciclista.java
@@ -0,0 +1,43 @@
+
+public abstract class Ciclista {
+
+    private int identificador;
+    private String nombre;
+    private int tiempoAcumulado=0;
+    public Ciclista(int identificador, String nombre) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+    }
+    
+    abstract String ImprimirTipo();
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTiempoAcumulado() {
+        return tiempoAcumulado;
+    }
+
+    public void setTiempoAcumulado(int tiempoAcumulado) {
+        this.tiempoAcumulado = tiempoAcumulado;
+    }
+    
+    protected void Imprimir(){
+        System.out.println("Identificador ="+identificador);
+        System.out.println("nombre ="+nombre);
+        System.out.println("Tiempo Acumulado="+tiempoAcumulado);
+    }
+}
